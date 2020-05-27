bab05c1b48fb9083a2137693a7d5cfd3f3b02fc2
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2020-05-27 16:18:00 -0700

    rudimentary optimizer

---
+++ optim.py
@@ -0,0 +1,23 @@
+""" An optimizer updates some parameters. """
+
+from typing import Iterable
+
+from ops import Variable
+
+
+class Optimizer:
+
+    def step(self) -> None:
+        raise NotImplementedError
+
+
+class SGD(Optimizer):
+
+    def __init__(self, params: Iterable[Variable], lr=1e-4):
+        self.lr = lr
+        self.step = 0
+        self.params = params
+
+    def step(self):
+        for param in self.params:
+            param.value -= self.lr * param.grad
