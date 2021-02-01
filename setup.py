6fcee4f93a7af17b6b3442ae3a57ad9140e9c835
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2021-02-01 11:57:32 -0800

    package name change

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="bcg",
+    name="edugrad",
     version="0.0.1",
     author="Shane Steinert-Threlkeld",
     author_email="ssshanest@gmail.com",
     description="Basic computation graph, for pedagogical purposes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/shanest/basic-computational-graph",
+    url="https://github.com/shanest/edugrad",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,6 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "numpy>=1.17"
+        "numpy>=1.17",
+        "networkx>=2.4"
     ]
 )
