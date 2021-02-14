2c426800de83128476ee5319d5fd05b5ba748971
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2021-02-14 13:04:13 -0800

    clean up example

--- examples/toy_half_sum/util.py
+++ examples/toy_half_sum/util.py
@@ -1,15 +1,1 @@
 """ Various helper functions. """
-
-import networkx as nx
-from networkx.drawing.nx_agraph import graphviz_layout
-import matplotlib.pyplot as plt
-
-
-def draw_graph(graph: nx.DiGraph) -> None:
-    nx.draw(
-        graph,
-        graphviz_layout(graph, prog="dot"),
-        labels={node: node.name for node in graph},
-    )
-    # TODO: write to file if specified
-    plt.show()
