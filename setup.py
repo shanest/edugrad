0db366ebad1efe348edc28c76385dace03cf3da6
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2021-02-15 16:28:43 -0800

    new version after major bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="edugrad",
-    version="0.0.7",
+    version="0.0.8",
     author="Shane Steinert-Threlkeld",
     author_email="ssshanest@gmail.com",
     description="Basic computation graph, for pedagogical purposes",
